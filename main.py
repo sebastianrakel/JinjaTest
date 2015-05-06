dbb877c1811ef73e29a27c7a537c128edd38a555
Sebastian <rakel.sebastian@lcs-landwehr.de>
2015-05-06 10:11:48 +0200

    add some comments

--- main.py
+++ main.py
@@ -9,17 +9,25 @@ env = Environment(loader=PackageLoader('main', 'templates'))
 def create_output(templates):
     output_path = os.path.join(basedir, 'output')
 
+    # check if output path exists, if delete it
     if os.path.exists(output_path):
         shutil.rmtree(output_path)
 
+    # create output path
     os.mkdir(output_path)
 
+    # copy static files to output
     shutil.copytree(os.path.join(basedir, 'static'), os.path.join(output_path, 'static'))
 
+    # for every template in directory render template and set values
     for template_file in templates:
+        # get template
         template = env.get_template(template_file)
+
+        # create stream with values
         template_stream = template.stream(templates[template_file])
 
+        # dump html to output path
         template_stream.dump(os.path.join(output_path, template_file))
 
 
