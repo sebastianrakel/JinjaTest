b11ad84a8d198e68d3cde62b42c0401485d7d558
Sebastian <rakel.sebastian@lcs-landwehr.de>
2015-05-06 10:00:23 +0200

    first jinja2 test

---
+++ main.py
@@ -0,0 +1,31 @@
+from jinja2 import Environment, PackageLoader
+import shutil
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+env = Environment(loader=PackageLoader('main', 'templates'))
+
+def create_output(templates):
+    output_path = os.path.join(basedir, 'output')
+
+    if os.path.exists(output_path):
+        shutil.rmtree(output_path)
+
+    os.mkdir(output_path)
+
+    shutil.copytree(os.path.join(basedir, 'static'), os.path.join(output_path, 'static'))
+
+    for template_file in templates:
+        template = env.get_template(template_file)
+        template_stream = template.stream(templates[template_file])
+
+        template_stream.dump(os.path.join(output_path, template_file))
+
+
+if __name__ == '__main__':
+    templates = {}
+
+    templates['index.html'] = {'name': 'Sebastian Rakel'}
+
+    create_output(templates)
